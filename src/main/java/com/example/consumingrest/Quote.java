13d973dfebbe1b0fbbeecc536d7eea66acb41ccb
vosmerkin <vosmerkin@mail.ru>
2022-04-20 19:12:21 +0300

    first commit

---
+++ src/main/java/com/example/consumingrest/Quote.java
@@ -0,0 +1,40 @@
+package com.example.consumingrest;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Quote {
+
+    private String type;
+    private Value value;
+
+    public Quote() {
+    }
+
+    @JsonProperty("typeOfResult")
+    public String getType() {
+        return type;
+    }
+
+    @JsonProperty("typeOfResult")
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Value getValue() {
+        return value;
+    }
+
+    public void setValue(Value value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Quote{" +
+                "type='" + type + '\'' +
+                ", value=" + value +
+                '}';
+    }
+}
